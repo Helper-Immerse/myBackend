7f1ea3e498cf8f5a39e33dd127c8b4cd4b4cb7b0
HelperImmerse <helperimmerse@gmail.com>
2022-10-15 09:49:51 +0530

    fifth project

--- src/Helper.js
+++ src/Helper.js
@@ -16,8 +16,21 @@ function myName () {
 array4 = [3,5,7,9,11,13,15,17,19,21]
 const tail = arr => (arr.length > 1 ? arr.slice(1) : arr);
 
+let A = [34, 35, 45, 48, 49];
+let B = [48, 55];
+let C = [34,67,23,55];
+let D = [22,37,89,49];
+let E = [22,67,45,34,29];
+let unions = [...new Set([...A, ...B,...C,...D,...E])];
+console.log(union);
+
+x = [[1,2],[3,4],[5,6]];
+o = x.reduce(function(prev,curr){prev[curr[0]]=curr[1];return prev;},{});
+
 module.exports.PresentDate = date
 module.exports.Monthme = currentMonth
 module.exports.BranchDe = BranchInfo
 module.exports.NameMy = myName
 module.exports.OddNumbers = tail
+module.exports.fiveArray = unions
+module.exports.Objects = o;
--- src/routes/route.js
+++ src/routes/route.js
@@ -14,7 +14,9 @@ router.get('/test-me', function (req, res) {
     console.log("The value of the constant is ",helper.PresentDate);
     console.log("The value of the constant is ",helper.Monthme);
     console.log("The value of the constant is ",helper.BranchDe);
-    console.log("The value of the constant is ",helper.OddNumbers);
+    console.log("The value of the constant is ",helper.OddNumbers());
+    console.log("The value of the constant is ",helper.fiveArray);
+    console.log("The value of the constant is ",helper.Objects);
     //Trying to use an external package called underscore
     let myArray = ['Akash', 'Pritesh', 'Sabiha']
     let result = underscore.first(myArray)
