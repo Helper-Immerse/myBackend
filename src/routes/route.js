2139b4deee381d7c46b49ddd3d8f9a60cbb8e353
HelperImmerse <helperimmerse@gmail.com>
2022-10-17 21:56:20 +0530

    myAssignment

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,19 +1,61 @@
 const express = require('express');
-const abc = require('../introduction/intro')
 const router = express.Router();
 
-router.get('/test-me', function (req, res) {
-    console.log('My batch is', abc.name)
-    abc.printName()
-    res.send('My second ever api!')
-});
 
-router.get('/students', function (req, res){
-    console.log("The path params in the request are : ", req.params)
-    let students = ['Sabiha', 'Neha', 'Akash']
-    res.send(students)
+
+
+let myArray = ["Kantara","Ramayana","HankyPanky","Nayakan","JaiBhim"]
+
+// ------------- Problem 1 Movies APi ------------ 
+router.get('/Movies', function (req, res) {
+    res.send(myArray)
 })
 
+// --------- Problem 2 Index number ---------
+// router.get('/Movies/:indexNumber',(req,res)=>{
+//     let index = req.params.indexNumber  
+//     res.send(myArray[index])
+// })
+
+// -------- Problem 3 error --------
+router.get('/Movies/:indexNumber', (req,res) => {
+    let index = req.params.indexNumber
+    if(index>myArray.length){
+        res.send('Please use a valid index')
+    }
+    else{
+        res.send(myArray[index])  
+    }
+})
+
+// ---------Problem4 stead of an array of strings define an array of movie objects this time
+let films = [ {
+    'id': 1,
+    'name': 'The Shining'
+   }, {
+    'id': 2,
+    'name': 'Incendies'
+   }, {
+    'id': 3,
+    'name': 'Rang de Basanti'
+   }, {
+    'id': 4,
+    'name': 'Finding Nemo'
+   }]
+//    router.get('/films', (req, res) => {
+//     res.send(films)
+// })
+
+// Problem 5
+router.get('/films/:filmid',(req,res)=>{
+    let filmid = req.params.filmid
+    if (filmid>films.length){
+        res.send('Please use a valid index')
+    }
+    else{
+        res.send(films[filmid-1])
+    }
+})
 
 // Example 1 for path params
 router.get('/students/:studentName', function(req, res){
