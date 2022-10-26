ac4d24536d1d6feac2ca0b0863866a566837109f
HelperImmerse <helperimmerse@gmail.com>
2022-10-26 18:56:30 +0530

    25 October

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,19 +1,32 @@
 const express = require('express');
 const router = express.Router();
+
+
+// =================Assignment========================
+const bookingData = require("../controllers/book1controler");
+router.post("/createBookData", bookingData.createBooksData);
+router.post("/createAuthorData", bookingData.createAuthorsData);
+router.get("/getAllData", bookingData.getAllData);
+router.get("/getResult", bookingData.getResult);
+router.get("/findBook", bookingData.findBook);
+
+// ========================================================
+// const bookmodel1
+// const bookmodel = require("../models/bookModel")
 // const UserModel= require("../models/userModel.js")
-const UserController= require("../controllers/userController")
-const BookController= require("../controllers/bookController")
+// const UserController= require("../controllers/userController")
+// const BookController= require("../controllers/bookController")
 
 router.get("/test-me", function (req, res) {
     res.send("My first ever api!")
 })
 
-router.post("/createUser", UserController.createUser  )
+// router.post("/createUser", UserController.createUser  )
 
-router.get("/getUsersData", UserController.getUsersData)
+// router.get("/getUsersData", UserController.getUsersData)
 
-router.post("/createBook", BookController.createBook  )
+// router.post("/createBook", BookController.createBook  )
 
-router.get("/getBooksData", BookController.getBooksData)
+// router.get("/getBooksData", BookController.getBooksData)
 
 module.exports = router;
