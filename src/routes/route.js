79c79ea36e270dac632aee36454d72bd21f06d63
HelperImmerse <helperimmerse@gmail.com>
2022-11-07 09:06:47 +0530

    auth2

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,20 +1,15 @@
 const express = require('express');
 const router = express.Router();
 const userController= require("../controllers/userController")
+const Middleware=require("../middleware/auth")
 
-router.get("/test-me", function (req, res) {
-    res.send("My first ever api!")
-})
+router.post("/createUser", userController.createUser  )
+router.post("/userLogin", userController.loginUser)
+router.get("/getUserData/:userId",Middleware.validationToken, userController.getUserData)
+router.put("/updateUser/:userId",Middleware.validationToken, userController.updateUser)
+router.delete("/deleteUser/:userId",Middleware.validationToken, userController.deleteUser)
+router.post("/users/:userId/posts",Middleware.validationToken, userController.postMessage)
 
-router.post("/users", userController.createUser)
 
-router.post("/login", userController.loginUser)
-
-//The userId is sent by front end
-router.get("/users/:userId", userController.getUserData)
-router.post("/users/:userId/posts", userController.postMessage)
-
-router.put("/users/:userId", userController.updateUser)
-router.delete('/users/:userId', userController.deleteUser)
 
 module.exports = router;
